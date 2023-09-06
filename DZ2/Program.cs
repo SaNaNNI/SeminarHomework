886415233f29dd629d1c005e285332b51acb35b2
SaNaNNI <138071039+SaNaNNI@users.noreply.github.com>
2023-09-06 23:24:48 +0200

    new files uploaded

---
+++ DZ2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write ("enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("enter third number: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+   max = c; 
+}
+
+Console.Write ($"Max number: {max}");
+
